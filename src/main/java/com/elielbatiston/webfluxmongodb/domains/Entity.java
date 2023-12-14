25b5e66a47b18beb36c0c67c71056c2ede3cd3ff
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-14 13:02:38 -0300

    add domains

---
+++ src/main/java/com/elielbatiston/webfluxmongodb/domains/Entity.java
@@ -0,0 +1,27 @@
+package com.elielbatiston.webfluxmongodb.domains;
+
+import java.util.Objects;
+
+public abstract class Entity {
+	private String id;
+
+	public Entity(final String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Entity entity)) return false;
+		return Objects.equals(id, entity.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+}
---
+++ src/main/java/com/elielbatiston/webfluxmongodb/domains/Wishlist.java
@@ -0,0 +1,41 @@
+package com.elielbatiston.webfluxmongodb.domains;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Wishlist extends Entity {
+
+	private Customer customer;
+	private Set<Product> products;
+
+	public Wishlist(final String id, final Customer customer) {
+		super(id);
+		this.customer = customer;
+	}
+
+	public Wishlist(final Customer customer) {
+		this(null, customer);
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public Set<Product> getProducts() {
+		if (this.products == null) {
+			this.products = new HashSet<>();
+		}
+		return this.products;
+	}
+
+	public void addProduct(final Product product) {
+		if (this.products == null) {
+			this.products = new HashSet<>();
+		}
+		this.products.add(product);
+	}
+
+	public void removeProduct(final Product product) {
+		this.getProducts().remove(product);
+	}
+}
